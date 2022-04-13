3aa6e88a37d47bf39d05f873a89f51a23c9f7be2
e_ginger <eugenia-golovina@yandex.ru>
2022-04-13 19:59:45 +0300

    Начинаю разработку приложения

--- src/app.py
+++ src/app.py
@@ -4,6 +4,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/features')
+def features_page():
+    return render_template('features.html')
+
+
 @app.route('/')
 def main_page():
     return render_template('main.html')
