1621b19648fb275afd4227e457156a34b490e034
e_ginger <eugenia-golovina@yandex.ru>
2022-04-14 12:15:36 +0300

    Основная функциональность приложения

--- src/app.py
+++ src/app.py
@@ -1,101 +1,219 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import pickle
 
-
-# Признаки
-# В реальном проекте я бы заморочилась и подобрала информативные имена
-features = {
-    'var1': 'Соотношение матрица-наполнитель',
-    'var2': 'Плотность, кг/м3',
-    'var3': 'Модуль упругости, ГПа',
-    'var4': 'Количество отвердителя, м.%',
-    'var5': 'Содержание эпоксидных групп,%_2',
-    'var6': 'Температура вспышки, С_2',
-    'var7': 'Поверхностная плотность, г/м2',
-    'var8': 'Модуль упругости при растяжении, ГПа',
-    'var9': 'Прочность при растяжении, МПа',
-    'var10': 'Потребление смолы, г/м2',
-    'var11': 'Угол нашивки, град',
-    'var12': 'Шаг нашивки',
-    'var13': 'Плотность нашивки'
-}
+MODELS_PATH = '../models/'
 
 
 # Функция для извлечения признаков из данных формы
-def get_data_from_form(params):
+def get_data_from_form(features, params):
+    param_names = features.keys()
+    data = dict.fromkeys(param_names, None)
     error = ''
-    data = {}
     # Преобразование из строк в числа
-    for param_name in features.keys():
-        pvalue = params.get(param_name)
-        if pvalue is not None and pvalue.strip(' \t') != '':
+    for param_name, param_value in params.items():
+        if param_value.strip(' \t') != '':
             try:
-                data[param_name] = float(pvalue)
+                data[param_name] = float(param_value)
             except:
-                error += f'{features[param_name]} - некорректное значение "{pvalue}"\n'
-        else:
-            error += f'{features[param_name]} - значение отсутствует\n'
+                error += f'{features[param_name]} - некорректное значение "{param_value}"\n'
     # Проверка допустимых диапазонов
     # Соотношение матрица-наполнитель (0..6)
-    if data.get('var1') and (data['var1'] < 0 or data['var1'] > 6):
-        error += f'{features["var1"]} - значение вне корректного диапазона\n'
+    if 'var1' in data and data['var1'] is not None:
+        if data['var1'] < 0 or data['var1'] > 6:
+            error += f'{features["var1"]} - значение вне корректного диапазона\n'
     # Плотность, кг/м3 (1700...2300)
-    if data.get('var2') and (data['var2'] < 1700 or data['var2'] > 2300):
-        error += f'{features["var2"]} - значение вне корректного диапазона\n'
+    if 'var2' in data and data['var2'] is not None:
+        if data['var2'] < 1700 or data['var2'] > 2300:
+            error += f'{features["var2"]} - значение вне корректного диапазона\n'
     # Модуль упругости, ГПа (2...2000)
-    if data.get('var3') and (data['var3'] < 2 or data['var3'] > 2000):
-        error += f'{features["var3"]} - значение вне корректного диапазона\n'
+    if 'var3' in data and data['var3'] is not None:
+        if data['var3'] < 2 or data['var3'] > 2000:
+            error += f'{features["var3"]} - значение вне корректного диапазона\n'
     # 'Количество отвердителя, м.% (17...200)
-    if data.get('var4') and (data['var4'] < 17 or data['var4'] > 200):
-        error += f'{features["var4"]} - значение вне корректного диапазона\n'
+    if 'var4' in data and data['var4'] is not None:
+        if data['var4'] < 17 or data['var4'] > 200:
+            error += f'{features["var4"]} - значение вне корректного диапазона\n'
     # Содержание эпоксидных групп,%_2 (14...34)
-    if data.get('var5') and (data['var5'] < 14 or data['var5'] > 34):
-        error += f'{features["var5"]} - значение вне корректного диапазона\n'
+    if 'var5' in data and data['var5'] is not None:
+        if data['var5'] < 14 or data['var5'] > 34:
+            error += f'{features["var5"]} - значение вне корректного диапазона\n'
     # Температура вспышки, С_2 (100...414)
-    if data.get('var6') and (data['var6'] < 100 or data['var6'] > 414):
-        error += f'{features["var6"]} - значение вне корректного диапазона\n'
+    if 'var6' in data and data['var6'] is not None:
+        if data['var6'] < 100 or data['var6'] > 414:
+            error += f'{features["var6"]} - значение вне корректного диапазона\n'
     # Поверхностная плотность, г/м2 (0.6...1400)
-    if data.get('var7') and (data['var7'] < 0.6 or data['var7'] > 1400):
-        error += f'{features["var7"]} - значение вне корректного диапазона\n'
+    if 'var7' in data and data['var7'] is not None:
+        if data['var7'] < 0.6 or data['var7'] > 1400:
+            error += f'{features["var7"]} - значение вне корректного диапазона\n'
     # Модуль упругости при растяжении, ГПа (64...83)
-    if data.get('var8') and (data['var8'] < 64 or data['var8'] > 83):
-        error += f'{features["var8"]} - значение вне корректного диапазона\n'
+    if 'var8' in data and data['var8'] is not None:
+        if data['var8'] < 64 or data['var8'] > 83:
+            error += f'{features["var8"]} - значение вне корректного диапазона\n'
     # Прочность при растяжении, МПа (1036...3849)
-    if data.get('var9') and (data['var9'] < 1036 or data['var9'] > 3849):
-        error += f'{features["var9"]} - значение вне корректного диапазона\n'
+    if 'var9' in data and data['var9'] is not None:
+        if data['var9'] < 1036 or data['var9'] > 3849:
+            error += f'{features["var9"]} - значение вне корректного диапазона\n'
     # Потребление смолы, г/м2 (33...414)
-    if data.get('var10') and (data['var10'] < 33 or data['var10'] > 414):
-        error += f'{features["var10"]} - значение вне корректного диапазона\n'
+    if 'var10' in data and data['var10'] is not None:
+        if data['var10'] < 33 or data['var10'] > 414:
+            error += f'{features["var10"]} - значение вне корректного диапазона\n'
     # Угол нашивки, град(0 или 90)
-    if data.get('var11') and (data['var11'] != 0.0 and data['var11'] != 90.0):
-        error += f'{features["var11"]} - значение вне корректного диапазона\n'
+    if 'var11' in data and data['var11'] is not None:
+        if data['var11'] != 0.0 and data['var11'] != 90.0:
+            error += f'{features["var11"]} - значение вне корректного диапазона\n'
     # Шаг нашивки(0...15)
-    if data.get('var12') and (data['var12'] < 0 or data['var12'] > 15):
-        error += f'{features["var12"]} - значение вне корректного диапазона\n'
+    if 'var12' in data and data['var12'] is not None:
+        if data['var12'] < 0 or data['var12'] > 15:
+            error += f'{features["var12"]} - значение вне корректного диапазона\n'
     # Плотность нашивки (0...104)
-    if data.get('var13') and (data['var13'] < 0 or data['var13'] > 104):
-        error += f'{features["var13"]} - значение вне корректного диапазона\n'
+    if 'var13' in data and data['var13'] is not None:
+        if data['var13'] < 0 or data['var13'] > 104:
+            error += f'{features["var13"]} - значение вне корректного диапазона\n'
+    # Проверка отсутствующих значений
+    if None in data.values():
+        error += f'Некоторые значения отсутствуют!\n'
     return data, error
+
+
+# Загрузка объекта pickle
+def load_pickle_obj(filename):
+    file = open(MODELS_PATH + filename, 'rb')
+    obj = pickle.load(file)
+    file.close()
+    return obj
 
 
 app = Flask(__name__)
 
 
-@app.route('/features', methods=['post', 'get'])
+@app.route('/features/', methods=['post', 'get'])
 def features_page():
-    # params = dict.fromkeys(features.keys(), '')
+    # Необходимые признаки
+    features = {
+        'var1': 'Соотношение матрица-наполнитель',
+        'var2': 'Плотность, кг/м3',
+        'var3': 'модуль упругости, ГПа',
+        'var4': 'Количество отвердителя, м.%',
+        'var5': 'Содержание эпоксидных групп,%_2',
+        'var6': 'Температура вспышки, С_2',
+        'var7': 'Поверхностная плотность, г/м2',
+        'var8': 'Модуль упругости при растяжении, ГПа',
+        'var9': 'Прочность при растяжении, МПа',
+        'var10': 'Потребление смолы, г/м2',
+        'var11': 'Угол нашивки, град',
+        'var12': 'Шаг нашивки',
+        'var13': 'Плотность нашивки'
+    }
+    # Переменные для формы
+    # params = {'var1': '', 'var2': '', 'var3': '', 'var4': '', 'var5': '', 'var6': '', 'var7': '',
+    #           'var8': '', 'var9': '', 'var10': '', 'var11': '', 'var12': '', 'var13': ''}
     params = {'var1': '3', 'var2': '2000', 'var3': '1999', 'var4': '95', 'var5': '25', 'var6': '255', 'var7': '720',
               'var8': '70', 'var9': '2300', 'var10': '180', 'var11': '0', 'var12': '8', 'var13': '52'}
     error = ''
     result = ''
+    # Получены данные из формы
     if request.method == 'POST':
         params = request.form.to_dict()
-        data, error = get_data_from_form(params)
+        data, error = get_data_from_form(features, params)
         if error == '':
+            # Входные данные корректны, выполняется логика
             x = pd.DataFrame(data, index=[0])
             x.columns = list(features.values())
             result = str(x.to_html())
+    # Отображение результата
     return render_template('features.html', params=params, error=error, result=result)
+
+
+@app.route('/model_1_2/', methods=['post', 'get'])
+def model_1_2_page():
+    # Необходимые признаки
+    features = {
+        'var1': 'Соотношение матрица-наполнитель',
+        'var2': 'Плотность, кг/м3',
+        'var3': 'модуль упругости, ГПа',
+        'var4': 'Количество отвердителя, м.%',
+        'var5': 'Содержание эпоксидных групп,%_2',
+        'var6': 'Температура вспышки, С_2',
+        'var7': 'Поверхностная плотность, г/м2',
+        'var10': 'Потребление смолы, г/м2',
+        'var11': 'Угол нашивки, град',
+        'var12': 'Шаг нашивки',
+        'var13': 'Плотность нашивки'
+    }
+    # Переменные для формы
+    # params = {'var1': '', 'var2': '', 'var3': '', 'var4': '', 'var5': '', 'var6': '', 'var7': '',
+    #           'var10': '', 'var11': '', 'var12': '', 'var13': ''}
+    # тестовый пример 19     var8=70 var9=3000
+    params = {'var1': '3.55', 'var2': '1880', 'var3': '313', 'var4': '129', 'var5': '21.25', 'var6': '300', 'var7': '210',
+              'var10': '220', 'var11': '0', 'var12': '10', 'var13': '80'}
+    error = ''
+    var8 = ''
+    var9 = ''
+    # Получены данные из формы
+    if request.method == 'POST':
+        params = request.form.to_dict()
+        data, error = get_data_from_form(features, params)
+        if error == '':
+            # Входные данные корректны, выполняется логика
+            x = pd.DataFrame(data, index=[0])
+            x.columns = list(features.values())
+            # для модуля упругости при растяжении
+            preprocessor1 = load_pickle_obj('preprocessor1')
+            model1 = load_pickle_obj('model1_best')
+            x1 = preprocessor1.transform(x)
+            y1 = model1.predict(x)
+            var8 = y1[0]
+            # для прочности при растяжении
+            preprocessor2 = load_pickle_obj('preprocessor2')
+            model2 = load_pickle_obj('model2_best')
+            x2 = preprocessor2.transform(x)
+            y2 = model2.predict(x)
+            var9 = y2[0]
+    # Отображение результата
+    return render_template('model_1_2.html', params=params, error=error, var8=var8, var9=var9)
+
+
+@app.route('/model_3/', methods=['post', 'get'])
+def model_3_page():
+    # Необходимые признаки
+    features = {
+        'var2': 'Плотность, кг/м3',
+        'var3': 'модуль упругости, ГПа',
+        'var4': 'Количество отвердителя, м.%',
+        'var5': 'Содержание эпоксидных групп,%_2',
+        'var6': 'Температура вспышки, С_2',
+        'var7': 'Поверхностная плотность, г/м2',
+        'var8': 'Модуль упругости при растяжении, ГПа',
+        'var9': 'Прочность при растяжении, МПа',
+        'var10': 'Потребление смолы, г/м2',
+        'var11': 'Угол нашивки, град',
+        'var12': 'Шаг нашивки',
+        'var13': 'Плотность нашивки'
+    }
+    # Переменные для формы
+    # params = {'var2': '', 'var3': '', 'var4': '', 'var5': '', 'var6': '', 'var7': '',
+    #           'var8': '', 'var9': '', 'var10': '', 'var11': '', 'var12': '', 'var13': ''}
+    params = {'var2': '2000', 'var3': '1999', 'var4': '95', 'var5': '25', 'var6': '255', 'var7': '720',
+              'var8': '70', 'var9': '2300', 'var10': '180', 'var11': '0', 'var12': '8', 'var13': '52'}
+    error = ''
+    var1 = ''
+    # Получены данные из формы
+    if request.method == 'POST':
+        params = request.form.to_dict()
+        data, error = get_data_from_form(features, params)
+        if error == '':
+            # Входные данные корректны, выполняется логика
+            x = pd.DataFrame(data, index=[0])
+            x.columns = list(features.values())
+            # для соотношения матрица-наполнитель
+            preprocessor3 = load_pickle_obj('preprocessor3')
+            model3 = load_pickle_obj('model3_base')
+            x3 = preprocessor3.transform(x)
+            y3 = model3.predict(x)
+            var1 = y3[0]
+    # Отображение результата
+    return render_template('model_3.html', params=params, error=error, var1=var1)
 
 
 @app.route('/')
